b7d061ddaf3a19e60305f3d8a8b4dfc67edec810
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-10-02 17:24:25 -0700

    Get by ID is now working with all appropriate errors.

--- routers/express-router.js
+++ routers/express-router.js
@@ -5,12 +5,31 @@ router = express.Router();
 router.get('/api/posts', (req, res) => {
     database.find()
     .then(response => {
-        res.status(200).json(response);
+        return res.status(200).json(response);
     })
     .catch(error => {
         console.log(error)
-        res.status(500).json({
-            errorMessage: "A server error occured"
+        return res.status(500).json({
+            error: "The posts information could not be retrieved."
+        })
+    })
+})
+
+router.get('/api/posts/:id', (req, res) => {
+    database.findById(req.params.id)
+    .then(response => {
+        if(response[0].id) {
+           return res.status(200).json(response)
+        } else {
+            return res.status(404).json({
+                errorMessage: "This post was either deleted or lost"
+            })
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        return res.status(500).json({
+            error: "The posts information could not be retrieved."
         })
     })
 })
